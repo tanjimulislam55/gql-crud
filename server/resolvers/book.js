f9509259dc1f8205a57b7eaddbd9a4a48e6a88e7
tanjimulislam55 <tanjimulislam118@gmail.com>
2022-04-10 11:45:40 +0600

    feat: mutation added for update and delete

--- server/resolvers/book.js
+++ server/resolvers/book.js
@@ -6,7 +6,6 @@ const totalBooks = () => books.length;
 const allBooks = () => books;
 
 const bookCreate = (parent, args, context) => {
-    console.log(args)
     // create a new object
     const book = {
         id: uuid(),
@@ -17,12 +16,37 @@ const bookCreate = (parent, args, context) => {
     return book
 }
 
+const bookUpdate = (parent, args, context) => {
+    const book = books.find(book => book.id === args.input.id)
+    if (!book) {
+        throw new Error("Id doesn't exist")
+    }
+    Object.keys(args.input).forEach((key) => {
+        book[key] = args.input[key]
+    })
+    return book
+}
+
+const bookDelete = (parent, args, context) => {
+    console.log(args)
+    const book = books.find(book => book.id === args.bookId)
+    if (!book) {
+        throw new Error("Id doesn't exist")
+    }
+    delete books[book]
+    return book
+    // const ok = Boolean(book)
+    // return { ok }
+}
+
 module.exports = {
     Query: {
         totalBooks,
         allBooks,
     },
     Mutation: {
-        bookCreate
+        bookCreate,
+        bookUpdate,
+        bookDelete
     }
 }
--- server/typeDefs/book.js
+++ server/typeDefs/book.js
@@ -7,16 +7,29 @@ module.exports = gql`
         title: String!
         author: String!
     }
-    type Query {
-        totalBooks: Int!
-        allBooks: [Book!]
-    }
-    input BookInput {
+    # input type
+    input BookCreateInput {
         title: String!
         author: String!
     }
+    # input type
+    input BookUpdateInput {
+        id: ID!
+        title: String
+        author: String
+    }
+    # type query
+    type Query {
+        totalBooks: Int!
+        allBooks: [Book!]
+        singleBook(bookId: String!): Book!
+        search(query: String): [Book]
+    }
+    # type mutation
     type Mutation {
         # bookCreate(title: String!, author: String!): Book!
-        bookCreate(input: BookInput!): Book!
+        bookCreate(input: BookCreateInput!): Book!
+        bookUpdate(input: BookUpdateInput!): Book!
+        bookDelete(bookId: ID!): Book!
     }
 `;
